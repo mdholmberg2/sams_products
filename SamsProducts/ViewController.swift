f5a12f92aea48b2db0ea902c3eeb539409cb7fe7
donHolmberg <mholmberg@austin.rr.com>
2019-10-02 12:03:37 -0500

    Initial Commit

---
+++ SamsProducts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SamsProducts
+//
+//  Created by M D Holmberg II on 10/2/19.
+//  Copyright © 2019 M D Holmberg II. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
